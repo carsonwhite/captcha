667f38f2b89ec61a644c821980163c9dc8e35b9b
Carson White <cwhite@mountwashington.ca>
2024-08-27 15:52:19 -0700

    basic captcha component

---
+++ src/lib/components/Captcha.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+ export let length = 6;
+
+ onMount (() => {
+  const canvas = document.getElementById("captchaCanvas") as HTMLCanvasElement;
+  const ctx = canvas.getContext("2d");
+
+  const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  const string_length = length;
+  let randomstring = "";
+
+  for (let i = 0; i < string_length; i++) {
+	const rnum = Math.floor(Math.random() * chars.length);
+	randomstring += chars.substring(rnum, rnum + 1);
+  }
+
+  ctx.font = "30px Arial";
+  ctx.fillText(randomstring, 10, 30);
+ });
+
+</script>
+
+<canvas id="captchaCanvas" width="200" height="50"></canvas>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
-hello world
+<script lang="ts">
+	import Captcha from "$lib/components/Captcha.svelte";
+
+</script>
+
+<Captcha length={6} />
